7fc2959c041bf3f92c66ad0cccef916107878941
Marianne <arroiomc@gmail.com>
2024-03-16 19:57:28 +0000

    Commit

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,13 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        shipping_fee_1,
         logcost,
         ship_cost
 
     from source
+     WHERE shipping_fee != shipping_fee_1
 
-),
+)
 
-select * from renamed;
-
+select * from renamed
