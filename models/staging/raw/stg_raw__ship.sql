93c6c5b1ca41d042726dbecd7be6d2e076a01007
Marianne <arroiomc@gmail.com>
2024-03-19 17:42:37 +0000

    Removendo coluna shipping_fee_1

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,12 +11,10 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
         logcost,
         ship_cost
 
     from source
-     WHERE shipping_fee != shipping_fee_1
 
 )
 
