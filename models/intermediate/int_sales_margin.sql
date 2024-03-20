1f5ac979addab097a53fdb462440995d429c7ce2
Marianne <arroiomc@gmail.com>
2024-03-20 00:04:30 +0000

    JOIN, purchase cost and margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,13 @@
 SELECT 
-    products_id, 
-    date_date, 
-    orders_id,
-    revenue, 
-    quantity, 
-    CAST(purchase_price AS FLOAT64), 
+   p.products_id, 
+   s.date_date, 
+   s.orders_id,
+   s.revenue, 
+   s.quantity, 
+   p.purchase_price AS purchase_price,
+   CAST(p.purchase_price AS FLOAT64)* CAST (s.quantity AS INT64),
     ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS purchase_cost,
-    s.revenue - ROUND(s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
+   ROUND(s.revenue - s.quantity*CAST(p.purchase_price AS FLOAT64),2) AS margin
 FROM {{ref("stg_raw__sales")}} s
-LEFT JOIN {{ref("stg_raw__products")}} p 
-    USING (products_id)
+LEFT JOIN {{ref("stg_raw__products")}} p ON p.products_id = s.products_id
+ 
